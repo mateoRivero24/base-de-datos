d4702ec68412bf43f0a135d26a9face69275f139
mateoRivero24 <riveromateo81@gmail.com>
2026-04-12 22:51:22 -0300

    Add files via upload

---
+++ Practica ts/eess.ts
@@ -0,0 +1,58 @@
+// Ejercicio 1
+/*
+let numero1 = Number(prompt("Ingrese un numero"))
+
+if(numero1 % 2 == 0){
+    alert("el numero es par")
+}else{
+    alert("el numero es impar")
+}
+*/
+
+
+// Ejercicio 2
+/*
+const aceleracion = Number(10)
+let velocidadi = Number(0)
+let distancia = Number(2000)
+let tiempo
+
+tiempo = Math.sqrt(distancia*2 / aceleracion)
+alert(tiempo)
+*/
+
+
+// Ejercicio 3
+/*
+let numero2 = Number(prompt("Ingrese el precio del juego"))
+alert(numero2*1.21)
+*/
+
+
+// Ejercicio 4
+/*
+let numero3 = Number(prompt("Ingrese el numero de mb"))
+alert(numero3*1024 + " kb")
+*/
+
+
+// Ejercicio 5
+/*
+let lados = Number(prompt("Ingrese el tamaño de los lados del cuadrado"))
+alert(lados*lados + " = area")
+*/
+
+
+// Ejercicio 6
+/*
+let hora = Number(prompt("Ingrese una hora"))
+alert("Pasaron " + (hora*60)*60 + " segundos") 
+
+*/
+
+// Ejercicio 7
+/*
+let cateto1 = Number(prompt("Ingrese el tamaño del cateto 1"))
+let cateto2 = Number(prompt("Ingrese el tamaño del cateto 2"))
+alert((cateto1*cateto2) / 2 + " = area")
+*/
